22db976d3dbec14ea25c7c22bf2b006c1b9810a5
terenceithaque <terence.lethierry@protonmail.com>
2023-04-01 21:45:23 +0200

    Ajout du personnage joueur et de ses contrôles

--- joueur.py
+++ joueur.py
@@ -5,8 +5,50 @@ import pygame # Importation de pygame
 
 class Joueur(pygame.sprite.Sprite):
     "Personnage joueur"
-    def __init__(self, image):
-        super.__init__()
-        self.image = pygame.image.load()
+    def __init__(self, image, width, height):
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+        print(self.image)
+        self.vies = 3
+        self.x = 0# Position x du joueur
+        self.y = 0 # Position y du joueur
+
+    def mettre_a_jour_position(self, key):
+        "Mettre à jour la position du joueur"
+
+
+        if key[pygame.K_UP]:    
+            self.y -= 5
+            print("y :", self.y)
+                
+            print(self.y)
+
+        if key[pygame.K_DOWN]:
+            self.y += 5
+            print("y :", self.y)
+            print(self.y)
+
+        if key[pygame.K_LEFT]:
+            self.x -= 5
+            print("x :", self.x)
+            print(self.x)
+
+        if key[pygame.K_RIGHT]:
+            self.x += 5
+            print("x :", self.x)
+            print(self.x)
+
+
+        self.rect.x = self.x
+        self.rect.y = self.y        
+
+
+
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))        
+
+
 
         
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import pygame # Importation du module pygame
 from decor import *
+from joueur import *
 
 pygame.init() # Initialisation du module pygame
 
@@ -15,6 +16,9 @@ vitesse_decor = 5
 
 decor = Decor(image_decor, decor_x, decor_y)
 
+image_joueur = "assets/joueur/joueur.png"
+joueur = Joueur(image_joueur, 50, 50)
+
 is_running = True # Le jeu est-il en cours d'exécution ?
 
 while is_running: # Tant que le jeu est exécuté
@@ -24,10 +28,17 @@ while is_running: # Tant que le jeu est exécuté
         if evenement == pygame.QUIT:
             is_running = False
 
+    touche_pressee = pygame.key.get_pressed()   # Verifier la touche pressée
+
+    joueur.mettre_a_jour_position(touche_pressee)
+
 
     # Dessiner les objets du jeu
     screen.fill((255,255,255))
     decor.draw(screen)
+
+    screen.blit(joueur.image, joueur.rect)
+
 
     # Mettre à jour l'écran
     pygame.display.flip()        
